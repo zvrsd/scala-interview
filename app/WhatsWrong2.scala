2ccec87177fe05546bb99499379a5115e1448520
zvrsd <65198569+zvrsd@users.noreply.github.com>
2020-10-30 15:52:24 +0100

    Completed WhatsWrong2

--- app/WhatsWrong2.scala
+++ app/WhatsWrong2.scala
@@ -29,6 +29,18 @@ object EnterpriseDao {
 object WhatsWrong2 {
 
   //Review this code. What could be done better ? How would you do it ?
+  /*
+   * Afaik Mark Zuckerberg is not the CEO of Google but im pretty sure that's not the point
+   *
+   * I'm not really sure on what could be done better, understanding this code was a bit tricky at first and im not
+   * comfortable enough with those Future Objects
+   * Maybe, since this seem to be an async task, we could include a callback method as a parameter,
+   * so all we will have to do is to provide the ceo_id and callback, and once the data has been
+   * obtained from the DAO, the callback function runs automatically
+   *
+   * In short, instead of returning this Future object, we add a callback method as a parameter
+   *
+   */
   def getCEOAndEnterprise(ceo_id: Option[String]): Future[(Option[CEO], Option[Enterprise])] = {
     for {
       ceo <- CEODao.byId(ceo_id.get)
