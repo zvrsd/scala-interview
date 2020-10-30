a7b27470e80b68c7b94dc1b2dd40a58244f20432
zvrsd <65198569+zvrsd@users.noreply.github.com>
2020-10-30 15:56:00 +0100

    Completed MapYourMap

--- app/MapYourMap.scala
+++ app/MapYourMap.scala
@@ -10,5 +10,33 @@ object MapYourMap {
   val devNames = Map("dev1" -> "Pierre", "dev2" -> "Remy", "dev3" -> "Noe", "dev4" -> "Alexandre")
   val devDepartments = Map("dev1" -> "analytics", "dev2" -> "frontend", "dev3" -> "api", "dev4" -> "frontend")
 
-  val namesInDepartments:Map[String, List[String]] = ???
+  val namesInDepartments:Map[String, List[String]] = getDevsAndDepartements(devNames, devDepartments)
+
+
+  // TODO : Sort the map
+  // Gets the departments and each dev associated to a specific one in a Map
+  def getDevsAndDepartements(devMap : Map[String,String], deptMap : Map[String,String] ): Map[String, List[String]] = {
+
+    var devDepartmentsPlus :Map[String, List[String]] = Map()
+
+    for ((key,value) <- deptMap) {
+      devDepartmentsPlus += (value -> getDevsByDepartment(value, devMap, deptMap))
+    }
+    devDepartmentsPlus
+  }
+
+  // Gets the devs associated to a specific department
+  def getDevsByDepartment(dept : String, devMap : Map[String,String], deptMap : Map[String,String] ) : List[String] = {
+
+    var devList : ListBuffer[String] = ListBuffer()
+
+    for ((key,value) <- deptMap) {
+
+      // If the departments match
+      if(value.equals(dept)){
+        devList += devMap(key)
+      }
+    }
+    devList.toList
+  }
 }
