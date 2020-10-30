aa6d315d5c34ac26eac46ff8600338589494fe4a
zvrsd <65198569+zvrsd@users.noreply.github.com>
2020-10-30 15:48:06 +0100

    Completed WhatsWrong1

--- app/WhatsWrong1.scala
+++ app/WhatsWrong1.scala
@@ -11,7 +11,25 @@ object WhatsWrong1 {
 
     override val city = "Paris"
   }
-
+  /*
+   * It prints "Paris"
+   */
   Supporter.city //What does this print ?
+
+  /*
+   * It prints "Ici c'est null !"
+   *
+   * Because when the support variable is set, it takes the content
+   * of city, which is null at that time. We could change city as much
+   * as we want, the support value will not change
+   *
+   * To overcome this issue, instead of getting the value from the support
+   * variable, we could dynamically get it, via a function
+   *
+   * def getSupport : String = {
+   *  s"Ici c'est $city !"
+   * }
+   *
+   */
   Supporter.support //What does this print and why ? How to fix it ?
 }
